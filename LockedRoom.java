58504e3ac5d37004854c6e54bb2d4455e6410a47
Shawn Norman <shawnnorman@gmail.com>
2013-11-19 19:09:56 -0800

    Initial commit

---
+++ LockedRoom.java
@@ -0,0 +1,18 @@
+
+/**
+ * A room that requires a key to enter.
+ * 
+ * @author Shawn Norman 
+ * @version 2013.02.26
+ */
+public class LockedRoom extends Room
+{
+    /**
+     * Constructor for objects of class LockedRoom
+     */
+    public LockedRoom(String description)
+    {
+        super(description);
+    }
+
+}
